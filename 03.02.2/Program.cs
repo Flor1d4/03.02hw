c41a57ee628cb07896c976146b71a02d4d09094e
Flor1d4 <lokikom3@gmail.com>
2023-02-09 22:54:48 +0100

    Add project files.

---
+++ 03.02.2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._02._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[5, 5];
+            Random rand = new Random();
+          
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    arr[i, j] = rand.Next(-100, 100);
+                }
+            }          
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            int minIndexA = 0;
+            int minIndexB = 0;
+            int maxIndexA = 0;
+            int maxIndexB = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (arr[i, j] < min)
+                    {
+                        min = arr[i, j];
+                        minIndexA = i;
+                        minIndexB = j;
+                    }
+                    if (arr[i, j] > max)
+                    {
+                        max = arr[i, j];
+                        maxIndexA = i;
+                        maxIndexB = j;
+                    }
+                }
+            }          
+           int sum = 0;
+
+            if (minIndexA < maxIndexA)
+            {
+                for (int i = minIndexA + 1; i < maxIndexA; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        sum += arr[i, j];
+                    }
+                }
+            }
+            else if (minIndexA > maxIndexA)
+            {
+                for (int i = maxIndexA + 1; i < minIndexA; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        sum += arr[i, j];
+                    }
+                }
+            }
+            else
+            {
+                int startIndex = Math.Min(minIndexB, maxIndexB) + 1;
+                int endIndex = Math.Max(minIndexB, maxIndexB) - 1;
+                for (int j = startIndex; j <= endIndex; j++)
+                {
+                    sum += arr[minIndexA, j];
+                }
+            }
+
+            Console.WriteLine("Сумма элементов между min и max: " + sum);
+        }
+    }
+}
---
+++ 03.02hw/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._02hw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            int[] A = new int[5];
+            int[,] B = new int[3, 4];
+            Random rand = new Random();
+
+            Console.WriteLine("Введите 5 элементов массива A через Enter:");
+            for (int i = 0; i < 5; i++)
+            {
+                A[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Создание чисел массива B ->");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    B[i, j] = rand.Next(1, 10);
+                }
+            }
+
+            Console.WriteLine("Массив A: " + string.Join(", ", A));
+
+            Console.WriteLine("Массив В в матричнйо форме:");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(B[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            int totVol = 3 * 4;
+            int minimum = int.MaxValue;
+            int totSum = 0;
+            int totProd = 1;
+            int evenCount = 0;
+            int oddSum = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (B[i, j] < minimum)
+                    {
+                        minimum = B[i, j];
+                    }
+                    totSum += B[i, j];
+                    totProd *= B[i, j];
+                }
+            }
+
+            foreach (int num in A)
+            {
+                if (num % 2 == 0)
+                {
+                    evenCount++;
+                }
+            }
+
+            for (int j = 0; j < 4; j++)
+            {
+                if (j % 2 != 0)
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        oddSum += B[i, j];
+                    }
+                }
+            }
+
+            Console.WriteLine("Общий объем массива B: " + totVol);
+            Console.WriteLine("Минимальный элемент массива B: " + minimum);
+            Console.WriteLine("Общая сумма всех элементов массива B: " + totSum);
+            Console.WriteLine("Суммарный продукт всех элементов массива B: " + totProd);
+            Console.WriteLine("Количество четных элементов в массиве A: " + evenCount);
+            Console.WriteLine("Сумма нечетных столбцов массива B: " + oddSum);
+            #endregion
+
+        }
+    }
+}
